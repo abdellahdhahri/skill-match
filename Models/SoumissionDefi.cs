208dbc4a0b7b1b8a9a84738f176f9ab3f8900813
abdellah <abdellahdhahri0@gmail.com>
2024-10-09 11:17:52 +0200

    defi classe update

--- Models/SoumissionDefi.cs
+++ Models/SoumissionDefi.cs
@@ -10,12 +10,6 @@
         public int CandidatId { get; set; } 
         public User Candidat { get; set; }  
 
-        public string FichierSoumis { get; set; } 
-
-        public int? RecruteurId { get; set; } 
-        public User Recruteur { get; set; }  
-
-        public float? Note { get; set; }  
-        public string Commentaire { get; set; }
+       
     }
 }
